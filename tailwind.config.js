f2ed05fa28f2caaadeddfb1775e46a733d35bcfb
NicolasAversa <nicolasaversadm@gmail.com>
2022-02-28 21:07:05 -0300

    API server initialized

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    fontFamily: {
+      sans: ['Inter', 'Comic Sans MS', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [],
